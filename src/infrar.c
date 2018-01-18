1d93f8b5b268ccacd9e7dac43595a74862de61b4
Your Name <you@example.com>
2018-01-18 22:51:13 +0800

    add watchdog

--- src/infrar.c
+++ src/infrar.c
@@ -10,8 +10,9 @@
 #define LED_SEL         P1SEL
 #define LED_OUT         P1OUT
 #define LED_DIR         P1DIR
-#define LED_N_PIN       BIT0
+#define LED_N_PIN       BIT3
 volatile int i=0;
+volatile uint8 softDog = 0;
 void __attribute__ ((interrupt(TIMER0_A1_VECTOR))) Timer_A (void)
 {  	
 	switch( TA0IV )	
@@ -20,11 +21,13 @@ void __attribute__ ((interrupt(TIMER0_A1_VECTOR))) Timer_A (void)
 		case  4:  break;
 		case 10:  
 			{
-				//i++;
-				//if (i == 2) {
-				//i=0;
-				__bic_SR_register_on_exit(LPM0_bits);			
-				//}
+				if (++softDog < 20)
+					WDTCTL = WDT_ARST_1000;				
+				i++;
+				if (i == 10) {
+				i=0;
+				__bic_SR_register_on_exit(LPM3_bits);			
+				}
 			}
 		break;
 	}
@@ -37,24 +40,32 @@ void task()
 	unsigned short len = 10;
 	LED_SEL &= ~LED_N_PIN;
 	LED_DIR |= LED_N_PIN;
+	LED_OUT |= LED_N_PIN;
+	__delay_cycles(500000);
 	LED_OUT &= ~LED_N_PIN;
+	__delay_cycles(500000);
+	LED_OUT |= LED_N_PIN;
 
 	radio_init();
-	TACTL = TASSEL_2 + MC_2 + TAIE + ID0;
+	CCR0 = 8192;
+	TACTL = TASSEL_1 + MC_1 + TAIE + ID0;
 	while (1) {
-		__bis_SR_register(LPM0_bits + GIE);
+		__bis_SR_register(LPM3_bits + GIE);
+		LED_OUT ^= LED_N_PIN;
 		if (i==10)
 		i=0;
 		len = 10;
-		memset(cmd+2,0x30+i,len);
+		/*memset(cmd+2,0x30+i,len);
 		cmd[0] = DEST_ADDR;
 		cmd[1] = SRC_ADDR;
-		len += 2;
+		len += 2;*/
+		memset(cmd,0x30+i,len);
 		radio_send(cmd,len);
-		radio_read(cmd1,&len);
-		if (memcmp(cmd+2,cmd1+2,10) !=0 || len != 12)
-			LED_OUT |= LED_N_PIN;		
+		//radio_read(cmd1,&len);
+		//if (memcmp(cmd,cmd1,10) !=0 || len != 10)
+		//	LED_OUT |= LED_N_PIN;		
 		radio_sleep();
+		softDog = 0;
 		i=i+1;
 	}
 	return ;
