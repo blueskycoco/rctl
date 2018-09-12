e1acc90e2a97d7ba7a3c903349e31597b253c90f
U-rdp-PC\rdp <rdp@rdp-PC.(none)>
2018-09-12 08:21:24 +0800

    update code

--- src/infrar.c
+++ src/infrar.c
@@ -481,15 +481,17 @@ void handle_timer()
 		handle_cc1101_cmd(CMD_ALARM,0x01);
 	}
 
-	if (heart_cnt >= 10800) {
+	if (heart_cnt >= 10810) {
 		heart_cnt = 0;
 		handle_cc1101_cmd(CMD_CUR_STATUS,0x00);	
 	}
 	if (low_power_cnt >= 10800) {
 		unsigned short bat = read_adc();
 		low_power_cnt = 0;
-		if (bat <= MIN_BAT)
+		if (bat <= MIN_BAT) {
 			handle_cc1101_cmd(CMD_LOW_POWER,0x00);
+			radio_sleep();
+		}
 	}
 }
 void task()
